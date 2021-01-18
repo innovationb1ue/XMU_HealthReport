847940a569f4e8d2c352584ac8df8527057dbc04
Jeff Blue <517262600@qq.com>
2021-01-18 10:26:49 +0800

    update
    update login method

--- Utils/login.py
+++ Utils/login.py
@@ -1,7 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import execjs
 
 Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+
+def encrypt(pwd, key):
+    with open('Utils/EncryptJS.js', 'r') as f:
+        js_str = f.read()
+    js = execjs.compile(js_str)
+    return js.call('encrypt_pass', pwd, key)
 
 def login_xmuxg(username:str, password:str):
     s = requests.Session()
@@ -9,7 +16,7 @@ def login_xmuxg(username:str, password:str):
     content = resp.content.decode('utf-8')
     soup = bs(content, 'lxml')
     form = {'dllt': 'userNamePasswordLogin', 'execution': 'e1s1', '_eventId': 'submit', 'rmShown': '1','rememberMe':'on',
-            'lt': soup.find('input', attrs={'name': 'lt'})['value'], 'username': username, 'password': password}
+            'lt': soup.find('input', attrs={'name': 'lt'})['value'], 'username': username, 'password': encrypt(password, soup.find('input',attrs={'id':'pwdDefaultEncryptSalt'})['value'])}
     s.post('https://ids.xmu.edu.cn/authserver/login?service=https://xmuxg.xmu.edu.cn/login/cas/xmu', data=form, headers=Headers)
     r1 = s.get('https://xmuxg.xmu.edu.cn/login/check', headers=Headers)
     try:
@@ -20,6 +27,8 @@ def login_xmuxg(username:str, password:str):
         print('登录失败.')
         print(str(e))
         return False
+
+
 if __name__ == '__main__':
     login_xmuxg('', '') # test method. input your name&pass here to run the test
 
